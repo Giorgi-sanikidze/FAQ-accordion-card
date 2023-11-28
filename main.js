52e99dfc70cfbeae5a964003cc2a142e23b3705c
giorgi2 <sanikidzegio50@gmail.com>
2023-11-28 14:14:16 +0400

    full project

---
+++ main.js
@@ -0,0 +1,58 @@
+let accordion = document.querySelectorAll(".accordion");
+let answers = document.querySelectorAll(".answer");
+
+
+
+    accordion.forEach((item) => {
+        const question = item.querySelector(".question")
+        const answer = item.querySelector(".answer");
+        const image = item.querySelector(".image");
+
+
+
+        question.addEventListener("click", () => {
+            if (answer.style.display == "block") {
+                answer.style.display = "none";
+                question.style.fontWeight = 'normal'
+            } else {
+                answer.style.display = "block";
+                question.style.fontWeight = 'bold'
+            }
+
+
+            if (image.style.rotate === "0deg") {
+                image.style.rotate = "180deg";
+            }
+            else {
+                image.style.rotate = "0deg";
+            }
+
+        })
+
+
+
+        image.addEventListener("click", () => {
+            if (answer.style.display == "block") {
+                answer.style.display = "none";
+                question.style.fontWeight = 'normal'
+            } else {
+                answer.style.display = "block";
+                question.style.fontWeight = 'bold'
+            }
+
+
+            if (image.style.rotate === "0deg") {
+                image.style.rotate = "180deg";
+            }
+            else {
+                image.style.rotate = "0deg";
+            }
+        
+        })
+    })
+
+
+
+
+
+
